ead80279238f21b3f7482167c253391d3fee5205
arthursouza-4f <joaoanton384@gmail.com>
2022-07-28 01:34:44 -0300

    Adicionado form para contratados, nao adiciona no banco ainda

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using EstacionamentoBenner.Models;
+using SIRP.Models;
 
-namespace EstacionamentoBenner.Controllers;
+namespace SIRP.Controllers;
 
 public class HomeController : Controller
 {
